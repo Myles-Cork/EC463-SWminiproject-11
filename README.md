fd0391d4f8bec6ec8bac55d5cb6cc749b959ab1a
Myles-Cork <mylescork@gmail.com>
2020-09-13 16:01:13 -0400

    Started database write api branch
    Started work on database and sensor write api. Need to upgrade firebase account to continue

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EC463-SWminiproject-11
+
+install extra dependencies:
+npm install --save express body-parser firebase-functions-helper
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,30 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import * as admin from 'firebase-admin';
+import * as firebaseHelper from 'firebase-functions-helper/dist';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+admin.initializeApp(functions.config().firebase);
+const database = admin.firestore();
+
+const app = express();
+const main = express();
+
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({extended: false}));
+main.use('api/v1', app);
+
+const deviceCollection = 'devices';
+export const webApi = functions.https.onRequest(main);
+
+app.patch('/device/:deviceId', async(req, res) =>{
+    try{
+      await firebaseHelper.firestore.updateDocument(database, deviceCollection, req.params.deviceId, req.body);
+      res.status(200).send('Update Success');
+    }catch(error){
+      res.status(204).send('Patch Error')
+    }
+})
