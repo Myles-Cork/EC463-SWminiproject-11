c36066b2aa89391d0e4e3e2cd279ac06e0290335
Myles-Cork <mylescork@gmail.com>
2020-09-15 12:47:33 -0400

    Update mocksensor.py
    Mock sensor now has current temp/humidity (updates faster than the array of previous temp/humidity values), and the arrays for temp and humidity can start "prewarmed" (initialized to normal temp and humidity values rather than all 0)

--- mocksensor/mocksensor.py
+++ mocksensor/mocksensor.py
@@ -6,36 +6,59 @@ import time
 print("This script sends mock data to the database under the deviceId specified\n")
 deviceId = input("Enter a deviceId: ")
 
+
 numPoints = 10 #number of datapoints to keep track of
-updateTime = 0.5 #seconds between updates
-numUpdates = 20 #total number of times to update (must be greater than numPoints for full array of generated data points)
+updateTime = 5 #seconds between current temp updates
+historyUpdateTime = 60 * 15 #seconds between history upates. History updates are when the arrays of previous temp/humidity values update
+historyUpdateIteration = historyUpdateTime/updateTime #number of iterations of current time updates inbetween history updates
+numUpdates = 100 #total number of times to update (must be greater than numPoints*historyUpdateIteration for a full cycle of generated data points through the arrays)
 
 #The following are for calculating new values for Temperature and Humidity
 tempmax = 80
 tempmin = 65
 humidmax= 50
 humidmin= 40
-tempshift = 2
-humidshift= 2
-prevtemp = 72
-prevhumid= 45
+tempshift = 1
+humidshift= 1
+prevtemp = random.randint(tempmin,tempmax) #aka the start temperature
+prevhumid= random.randint(humidmin,humidmax) #aka the start humidity
 
 Temperature = deque([0]*numPoints)
 Humidity = deque([0]*numPoints)
 
+prewarmAns = ' '
+while prewarmAns != 'Y' and prewarmAns != 'N':
+    print("Prewarm the history data? (Arrays start with a full array of randomly generated values)") #saves on function calls for graph testing
+    prewarmAns = input("Enter Y or N: ")
+
+if prewarmAns == 'Y':
+    for k in range(numPoints-1,-1,-1):
+        Temperature[k]= max(tempmin,  min(tempmax, prevtemp   + random.randint(-tempshift,tempshift)))
+        Humidity[k]   = max(humidmin, min(humidmax, prevhumid + random.randint(-humidshift,humidshift)))
+        prevtemp = Temperature[k]
+        prevhumid = Humidity[k]
+
 
 for i in range(0, numUpdates):
     start = time.process_time()
-    Temperature.rotate(1)
-    Humidity.rotate(1)
 
-    Temperature[0] = max(tempmin,  min(tempmax, prevtemp   + random.randint(-tempshift,tempshift)))   #new temperature is previous shifted randomly up to tempshift + or - and clamped between tempmin and tempmax
-    Humidity[0]    = max(humidmin, min(humidmax, prevhumid + random.randint(-humidshift,humidshift))) #new humidity is previous shifted randomly up to humidshift + or - and clamped between humidmin and humidmax
-    prevtemp = Temperature[0]
-    prevhumid = Humidity[0]
+    currentTemp = max(tempmin,  min(tempmax, prevtemp   + random.randint(-tempshift,tempshift)))   #new temperature is previous shifted randomly up to tempshift + or - and clamped between tempmin and tempmax
+    currentHumid= max(humidmin, min(humidmax, prevhumid + random.randint(-humidshift,humidshift))) #new humidity is previous shifted randomly up to humidshift + or - and clamped between humidmin and humidmax
+
+    prevtemp = currentTemp
+    prevhumid = currentHumid
+
+    if i%historyUpdateTime == 0:
+        Temperature.rotate(1)
+        Humidity.rotate(1)
+        Temperature[0] = currentTemp
+        Humidity[0] = currentHumid
+
 
     r = requests.patch('https://us-central1-ec463-swminiproject-11.cloudfunctions.net/webApi/api/v1/device/' + deviceId,
-        data ={
+        data = {
+            'CurrentTemperature' : int(currentTemp),
+            'CurrentHumidity' : currentHumid,
             'Temperature' : Temperature,
             'Humidity' : Humidity
         })
@@ -48,6 +71,7 @@ for i in range(0, numUpdates):
     sleepTime = max(0.0 , updateTime - (time.process_time() - start)) #ensures time between updates is the same as updateTime and/or non-negative
 
     time.sleep(sleepTime)
-    print("Update Time: " + sleepTime)
+    print("Update Time:")
+    print(sleepTime)
 
 print("Done")
